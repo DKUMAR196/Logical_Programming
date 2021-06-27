4f4cbe35925eccdc78a58fc907010335a7e29374
DKUMAR196 <84921216+DKUMAR196@users.noreply.github.com>
2021-06-27 20:15:43 +0530

    Day6_Logical_Programming

---
+++ Day6_Logical_Programming/src/com/Logical_Programming/Reverse_Number.java
@@ -0,0 +1,17 @@
+package com.Logical_Programming;
+
+public class Reverse_Number {
+	public static void main(String[] args) {
+
+	    int num = 1234, reversed = 0;
+	    while(num != 0) {
+	      int digit = num % 10;
+	      reversed = reversed * 10 + digit;
+	      num /= 10;
+	    }
+
+	    System.out.println("Reversed Number: " + reversed);
+	  }
+	}
+
+
---
+++ Day6_Logical_Programming/src/com/Logical_Programming/Simulate_Stopwatch_Program.java
@@ -0,0 +1,5 @@
+package com.Logical_Programming;
+
+public class Simulate_Stopwatch_Program {
+
+}
